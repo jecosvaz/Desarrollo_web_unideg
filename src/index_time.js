d9aaa245b971d17f9bed7616e66c3474f3d5eb19
jecosvaz <jesus.cosmev@alumnos.sabes.edu.mx>
2020-09-16 02:35:36 -0500

    Modicado tiempo y fecha

--- src/index_time.js
+++ src/index_time.js
@@ -1,43 +1,34 @@
 var actualizarHora = ()=>{
 	// Obtenemos la fecha actual, incluyendo las horas, minutos, segundos, dia de la semana, dia del mes, mes y año;
-	var fecha = new Date(),
+	let fecha = new Date(),
 		horas = fecha.getHours(),
-		ampm = ampm,
+		ampm = (' '),
 		minutos = fecha.getMinutes(),
 		segundos = fecha.getSeconds(),
 		diaSemana = fecha.getDay(),
 		dia = fecha.getDate(),
 		mes = fecha.getMonth(),
-		year = fecha.getFullYear(),
-		tiempo= `La hora es ${horas}:${minutos}:${segundos}`;
-		date= `El día de hoy es ${dia}, ${mes},${year}`
-	 console.log(date)
-
-	// Accedemos a los elementos del DOM para agr`egar mas adelante sus correspondientes valores
-	document.getElementById('tiempo').innerHTML= tiempo;  
-	document.getElementById('date').innerHTML=date;	
-
-	
+		year = fecha.getFullYear();
+			   	
 	// Obtenemos el dia se la semana y lo mostramos
-	var semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
-	diaSemana.textContent = semana[diaSemana];
-
+	let semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+	let DiadeLaSemana= semana[diaSemana];	
+	
 	// Obtenemos el dia del mes
 	dia.textContent = dia;
 
 	// Obtenemos el Mes y año y lo mostramos
 	var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-	mes.textContent = meses[mes];
-	year.textContent = year;
-
+	let mesActual= meses[mes];
+		
 	// Cambiamos las hora de 24 a 12 horas y establecemos si es AM o PM
-
 	if (horas >= 12) {
 		horas = horas - 12;
 		ampm = 'PM';
 	} else {
 		ampm = 'AM';
 	}
+	ampm.textContent = ampm;
 
 	// Detectamos cuando sean las 0 AM y transformamos a 12 AM
 	if (horas == 0 ){
@@ -45,16 +36,23 @@ var actualizarHora = ()=>{
 	}
 
 	// Si queremos mostrar un cero antes de las horas ejecutamos este condicional
-	// if (horas < 10){horas = '0' + horas;}
-	horas.textContent = horas;
-	ampm.textContent = ampm;
+	if (horas < 10){horas = '0' + horas;}
+	horas.textContent = horas;	
 
 	// Minutos y Segundos
-	if (minutos < 10){ minutos === "0" + minutos; }
-	if (segundos < 10){ segundos === "0" + segundos; }
+	if (minutos < 10){ minutos = `0${minutos}`;}
+	if (segundos < 10){ segundos = `0${segundos}`;}
 
 	minutos.textContent = minutos;
 	segundos.textContent = segundos;
+
+	// pintamos el día y la fecha actual 
+	let date= `El día de hoy es ${DiadeLaSemana} ${dia} ${mesActual} del año ${year} `;
+	let tiempo= `La hora actual es: ${horas}:${minutos}:${segundos} ${ampm}`;
+
+	// Accedemos a los elementos del DOM para agr`egar mas adelante sus correspondientes valores
+	document.getElementById('date').innerHTML=date;
+	document.getElementById('tiempo').innerHTML=tiempo;
 };
 
 actualizarHora();
